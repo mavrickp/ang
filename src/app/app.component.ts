ecd915f2b443e660ac335f04e039991c81e87017
mavrickp001 <mavrickp001@gmail.com>
2018-10-22 21:40:13 +0300

    check uer clain code snnippts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ademo';
+  final = [];
+  data = [
+    {
+      'id': 'dashboard',
+      'type': 'item'
+    },
+    {
+      'id': 'twitter',
+      'type': 'group',
+      'child': [
+        {
+          'id': 'search',
+          'type': 'item'
+        }
+      ]
+    }
+  ];
+  nav = [
+    {
+      'id': 'dashboard',
+      'type': 'item',
+      'title': 'DASHBOARD'
+    },
+    {
+      'id': 'twitter',
+      'type': 'group',
+      'title': 'TWITTER',
+      'child': [
+        {
+          'id': 'search',
+          'type': 'item',
+          'title': 'SEARCH'
+        },
+        {
+          'id': 'monitoring',
+          'type': 'item',
+          'title': 'MONITORING'
+        }
+      ]
+    }
+  ];
+
+  ngOnInit(): void {
+    for (const iterator of this.data) {
+      if (iterator.type === 'item') {
+        this.nav.filter((x) => x.id === iterator.id)
+        .map((x) => {
+          this.final.push({
+            'id': x.id,
+            'type': x.type,
+            'title': x.title
+          });
+        });
+        continue;
+      }
+      if (iterator.type === 'group') {
+        this.nav.filter((x) => x.id === iterator.id)
+        .map((x) => {
+          this.final.push({
+            'id': x.id,
+            'type': x.type,
+            'title': x.title,
+            'child': this.children(iterator.child, x.child)
+          });
+        });
+      }
+    }
+    console.log(this.final);
+    }
+
+    children(childData, childNav) {
+      let child:any[] = [];
+      for (const iterator of childData) {
+        if (iterator.type === 'item') {
+          childNav.filter((x) => x.id === iterator.id)
+          .map((x) => {
+            child.push({
+              'id': x.id,
+              'type': x.type,
+              'title': x.title
+            });
+          });
+          return child;
+        }
+    }
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { RcComponent } from './rc/rc.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    RcComponent
-  ],
+    AppComponent
+    ],
   imports: [
     BrowserModule
   ],
